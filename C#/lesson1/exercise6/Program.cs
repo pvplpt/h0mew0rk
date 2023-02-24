b36bf5dadc86a1c494f55ad57b0cd66b3dc1c416
Pavel Pozdnyakov <pvplpt@inbox.ru>
2023-02-24 14:16:14 +0500

    added solution of problem 6

--- C#/lesson1/exercise6/Program.cs
+++ C#/lesson1/exercise6/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+//
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+Console.Write("Введите целое число: ");
+int userNumber = int.Parse(Console.ReadLine() ?? "");
+
+string IsEvenNumber = "нет";
+
+if (userNumber % 2 == 0) IsEvenNumber = "да";
+
+Console.WriteLine($"{userNumber} -> {IsEvenNumber}");
