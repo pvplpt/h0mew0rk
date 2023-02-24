05c9d8b55d0a918a47ac63d96daf1a81a8c99116
Pavel Pozdnyakov <pvplpt@inbox.ru>
2023-02-24 13:29:44 +0500

    added solution of problem 2

--- C#/lesson1/task2/Program.cs
+++ C#/lesson1/task2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.Write("Введите первое целое число: ");
+int firstUserNumber = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите второе целое число: ");
+int secondUserNumber = int.Parse(Console.ReadLine() ?? "");
+
+int max = firstUserNumber;
+
+if (max < secondUserNumber) max = secondUserNumber;
+
+Console.WriteLine($"a = {firstUserNumber}, b = {secondUserNumber} -> max = {max}");
