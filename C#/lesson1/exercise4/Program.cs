fda7e0642ac56dce263af659a8ad4cbd1e60b779
Pavel Pozdnyakov <pvplpt@inbox.ru>
2023-02-24 13:53:01 +0500

    added solution of problem 4

--- C#/lesson1/exercise4/Program.cs
+++ C#/lesson1/exercise4/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и 
+//выдаёт максимальное из этих чисел.
+//
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Write("Введите первое целое число: ");
+int firstUserNumber = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите второе целое число: ");
+int secondUserNumber = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите третье целое число: ");
+int thirdUserNumber = int.Parse(Console.ReadLine() ?? "");
+
+int max = firstUserNumber;
+
+if (max < secondUserNumber) max = secondUserNumber;
+if (max < thirdUserNumber) max = thirdUserNumber;
+
+Console.WriteLine($"{firstUserNumber}, {secondUserNumber}, {thirdUserNumber} -> {max}");
