1b472dcf77f4bb1f84a039eae3aaaf5cb6e498c1
Pavel Pozdnyakov <pvplpt@inbox.ru>
2023-02-24 14:40:23 +0500

    added solution of problem 8

--- C#/lesson1/exercise8/Program.cs
+++ C#/lesson1/exercise8/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+//
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+
+Console.Write("Введите челое число: ");
+int userNumber = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write($"{userNumber} -> ");
+int evenNumber = 2;
+while (evenNumber <= userNumber) {
+  Console.Write($"{evenNumber} ");
+  evenNumber += 2;
+}
+Console.WriteLine();
