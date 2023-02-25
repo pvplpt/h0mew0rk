51babe2bfe11ea95628c321346513b546eac69cc
Pavel Pozdnyakov <pvplpt@inbox.ru>
2023-02-25 20:24:15 +0500

    added solution of problem 13

--- C#/lesson2/exercise13/Program.cs
+++ C#/lesson2/exercise13/Program.cs
@@ -7,3 +7,45 @@
 //32679 -> 6
 //=============================================================================
 
+//Функция выдает k-ую цифру натурального числа n 
+//или -1 если это не очень то и возможно
+int DigitOfNumber(int n, int k)
+{
+  //Текущая цифра
+  int digit = -1;
+  //Текущий разряд числа
+  int numberDigit = 0;
+  //Цикл работает только с положительными числами
+  //поэтому меняем знак для отрицательных чисел
+  if (n < 0) n *= -1;
+  //Цикл поиска подходящей цифры
+  while (n > 0 && numberDigit < k)
+  {
+    digit = n % 10;
+    numberDigit++;
+    n /= 10;
+  }
+  //выдаем -1 если запросили разряд числа, которого нету
+  if (k > numberDigit) return -1;
+  else return digit;
+}
+
+//Функция ввода целого числа 
+int InputIntegerNumber(string hint)
+{
+  Console.Write(hint);
+  return int.Parse(Console.ReadLine() ?? "");
+}
+
+//Функция вывода в консоль по шаблону дух целых чисел: 456 -> 5
+//или 78 -> третьей цифры нет
+void PrintThirdDigit(int a, int b)
+{
+  if (b == -1) Console.WriteLine($"{a} -> третьей цифры нет");
+  else Console.WriteLine($"{a} -> {b}");
+}
+
+
+//Основная программа
+int userNumber = InputIntegerNumber("Введите целое число: ");
+PrintThirdDigit(userNumber,DigitOfNumber(userNumber, 3));
