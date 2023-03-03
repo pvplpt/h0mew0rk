4c0e101b89fbbee8a21c66cd80c2ffdc7b2de6b4
Pavel Pozdnyakov <pvplpt@inbox.ru>
2023-03-04 01:17:41 +0500

    added solution of problem 25

--- C#/lesson4/exercise25/Program.cs
+++ C#/lesson4/exercise25/Program.cs
@@ -6,4 +6,58 @@
 //2, 4 -> 16
 //=============================================================================
 
+//Основная программа
+//Очистка консоли
+Console.Clear();
+//Ввод целых чисел
+int A = InputIntegerNumber("Введите целое число A: ", false);
+int B = InputIntegerNumber("Введите натуральное число B: ", true);
+//Возведение А в степень В
+int degree = PowerAB(A, B);
+//Вывод результата
+PrintResult(A, B, degree);
 
+
+//Функция ввода целого числа
+static int InputIntegerNumber(string msg, bool flagNatural)
+{
+  int num;
+  while (true)
+  {
+    try
+    {
+      Console.Write(msg);
+      num = int.Parse(Console.ReadLine() ?? "");
+      if (flagNatural)
+        if (num > 0) break;
+        else Console.WriteLine("Ошибка ввода натурального числа!");
+      else break;
+    }
+    catch (Exception exc)
+    {
+      Console.WriteLine($"Ошибка ввода! {exc.Message}");
+    }
+  }
+  return num;
+}
+
+
+//Функция возведения в натуральную степень
+static int PowerAB(int A, int B)
+{
+  int i = 0;
+  int result = 1;
+  while (i < B)
+  {
+    result *= A;
+    i++;
+  }
+  return result;
+}
+
+
+//Функция вывода в консоль по шаблону: 3, 5 -> 243
+static void PrintResult(int a, int b, int deg)
+{
+  Console.WriteLine($"{a}, {b} -> {deg}");
+}
