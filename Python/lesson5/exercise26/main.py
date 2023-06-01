2767d3d6a2888ced3b2c21a5761e4f38fd955c6e
Pavel Pozdnyakov <pvplpt@inbox.ru>
2023-06-01 19:14:30 +0500

    добавил решение задачи 26

--- Python/lesson5/exercise26/main.py
+++ Python/lesson5/exercise26/main.py
@@ -5,3 +5,13 @@
 A = 3; B = 5 -> 243 (3⁵)
     A = 2; B = 3 -> 8 
 """
+
+def PowerAB(a, b):
+    if b == 0:
+        return 1
+    return a * PowerAB(a, b - 1)
+
+
+A, B = (int(i) for i in input('Введите A и B: ').split())
+
+print(f'\nA = {A}; B = {B} -> {PowerAB(A,B)}')
