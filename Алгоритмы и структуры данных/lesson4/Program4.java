c1b6495977fa0a3afeb8c4938c3423dadd165c31
Pavel Pozdnyakov <pvplpt@inbox.ru>
2023-06-04 11:00:10 +0500

    добавил урок 4

---
+++ Алгоритмы и структуры данных/lesson4/Program4.java
@@ -0,0 +1,14 @@
+/*
+* Реализовать класс работающий по принципу HashMap. 
+* Включая внутренний массив узлов с индексацией по хешу и описание узла. 
+* Добавить в класс методы:
+*   Object put(Integer key , Integer value) добавить элемент
+*   Object get(Integer key) получить значение соответствующее ключу
+*   Object remove(Integer key) удалить элемент с соответствующем ключём
+*   Object replays(Integer key, Integer v) заменить значение
+*   int size() количество элементов
+*   boolean containsKey(Integer key) проверка наличия ключа и значения
+*   boolean containsValue(Integer v)
+ */
+
+ 
