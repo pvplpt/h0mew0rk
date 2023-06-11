d8337d7ad9b48544c7d5ec3d0a01c5f31db8878a
Pavel Pozdnyakov <pvplpt@inbox.ru>
2023-06-11 18:39:38 +0500

    добавил форматирование задачи 36

--- Python/lesson7/exercise36/main.py
+++ Python/lesson7/exercise36/main.py
@@ -18,12 +18,12 @@
 6 12 18 24 30 36
 """
 
+
 def print_operation_table(operation, num_rows=6, num_columns=6):
-    matrix = [[operation(i, j) for i in range(1, num_rows + 1)] for j in range(1, num_columns + 1)]
+    matrix = [[operation(i, j) for i in range(1, num_rows + 1)]
+              for j in range(1, num_columns + 1)]
     for row in matrix:
         print(*row)
-
-
 
 
 print_operation_table(lambda x, y: x * y)
