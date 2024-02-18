28fd98e35df70eeb57d1146ce51b8b2ef8e68e01
Pavel Pozdnyakov <pvplpt@inbox.ru>
2024-02-18 23:30:00 +0500

    добавил решение задачи 1 урока 7 курса массивы и указатели в С++

---
+++ Cpp/Массивы и Указатели в C++/Урок7/Задача1/main.cpp
@@ -0,0 +1,53 @@
+/* Задача 1
+
+Что нужно сделать:
+Напишите функцию swapvec, принимающую std::vector<int> и указатель на массив
+переменных типа int (одного размера) и обменивающую значения этих массивов.
+
+Пример:
+
+std::vector<int> a = {1,2,3,4};
+int b[] = {2,4,6,8};
+swapvec(a,b);
+for(int i = 0; i < 4; ++i)
+std::cout << a[i];
+std::cout << std::endl;
+for(int i = 0; i < 4; ++i)
+std::cout << b[i];
+
+Вывод:
+2468
+1234
+
+Чек-лист для проверки задачи:
+• Функция принимает корректные типы данных, тип возвращаемого значения — void.
+• Функция не использует библиотек кроме <iostream> и <vector>.
+• Функция меняет значения между объектами.
+• Вектор передаётся по ссылке.
+*/
+
+#include <iostream>
+#include <vector>
+
+void swapvec(std::vector<int> &a, int *b);
+
+int main(int argc, char const *argv[]) {
+    std::vector<int> a = {1, 2, 3, 4};
+    int b[] = {2, 4, 6, 8};
+    swapvec(a, b);
+    for (int i = 0; i < 4; ++i)
+        std::cout << a[i];
+    std::cout << std::endl;
+    for (int i = 0; i < 4; ++i)
+        std::cout << b[i];
+    std::cout << std::endl;
+    return 0;
+}
+
+void swapvec(std::vector<int> &a, int *b) {
+    for (int i = 0; i < a.size(); i++) {
+        int tmp = a[i];
+        a[i] = *(b + i);
+        *(b + i) = tmp;
+    }
+}
